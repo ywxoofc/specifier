daff68c474e5c54a45310a7a6468d09bd4ea32bc
ywxoofc <109625340+ywxoofc@users.noreply.github.com>
2025-12-21 02:52:28 +0100

    Update and rename WZ3.lua to WZ4.lua

--- WZ4.lua
+++ WZ4.lua
@@ -8,8 +8,8 @@ end
 g._AutoLoader = true
 print("[AutoLoader] Guard gesetzt")
 
-local MAIN = "https://raw.githubusercontent.com/ywxoofc/specifier/refs/heads/main/WZ1.lua"
-local AUTO = "https://raw.githubusercontent.com/ywxoofc/specifier/refs/heads/main/WZ3.lua"
+local MAIN = "https://raw.githubusercontent.com/ywxoofc/specifier/refs/heads/main/WZ2.lua"
+local AUTO = "https://raw.githubusercontent.com/ywxoofc/specifier/refs/heads/main/WZ4.lua"
 
 local function safeLoad(url)
     print("[AutoLoader] Lade:", url)
